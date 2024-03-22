2f1234acbb6911180361896b04f234a4368f0ebb
BeneX Drake <benex@hotmail.de>
2024-03-22 22:07:51 +0100

    testing cards

--- components/guild/card.tsx
+++ components/guild/card.tsx
@@ -5,20 +5,19 @@ export default function GuildCard(props: any) {
     let guild = props.guild;
 
     
-    let channels = guild.channels.map((x:any) => {return x.name}).join(' ,')
+    let channels = guild.channels.map((x:any) => {return '#'+x.name}).join(' ,')
 
   return (
     <Link href={'/guilds/'+guild.id} style={{textDecoration:'none'}}>
-      <div className={styles.card}>
-        <h3 className={styles.header}>{guild.name}</h3>
-      <img className={styles.image} src={'https://cdn.discordapp.com/icons/'+guild.id+'/'+guild.icon} alt="" />
-        <div className={styles.channels}>
-          <p>
-            {channels}
-            </p>
-        </div>
-        <p className={styles.description}>{guild.description}</p>
-      </div>
+
+<div className="card">
+  <div className="header">{guild.name}</div>
+  <img className={styles.image} src={'https://cdn.discordapp.com/icons/'+guild.id+'/'+guild.icon} alt="" />
+  <p className={styles.description}>{guild.description}</p>
+  <p>
+    {channels}
+  </p>
+</div>
       </Link>
   );
 }
